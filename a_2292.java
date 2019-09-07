9d7d8551ab504047c1140d18bfedc0e91562b011
leeheajin <43198796+leeheajin@users.noreply.github.com>
2019-09-08 05:32:53 +0900

    Add files via upload

---
+++ a_2292.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class a_2292 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // 1<=n<=1,000,000,000
+		
+		int i=1;
+		int num=1;
+		
+		if (n==1) {
+			System.out.println(i);
+		}
+		
+		else {
+			num++;
+			while (n>=num) {
+				num = num + (i*6);
+				i++;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
