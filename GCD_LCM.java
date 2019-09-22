d84a620fab6c9beef98c8082d5223ee6509c2dc2
leeheajin <43198796+leeheajin@users.noreply.github.com>
2019-09-22 21:30:38 +0900

    Add files via upload

---
+++ GCD_LCM.java
@@ -0,0 +1,52 @@
+package 함수;
+
+public class GCD_LCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int GCD (int a, int b) { // 최대 공약수
+		
+		int tmp1 = a, tmp2 = b;
+		int r = 1;
+		
+		if(a<b){ // b가 클 경우 큰수를 a으로 변경
+			a = tmp2;
+			b = tmp1;
+	         }
+	 
+		while (r>0) {
+			r = a % b;
+			a = b;
+			b = r;
+		}
+	        return a;
+	        
+	}
+	
+	public static int LCM (int a, int b) { // 최소공배수
+		
+		int tmp1 = a, tmp2 = b;
+		int r = 1;
+		int LCM;
+		
+		if(a<b){ // b가 클 경우 큰수를 a으로 변경
+			a = tmp2;
+			b = tmp1;
+	         }
+	 
+		while (r>0) {
+			r = a % b;
+			a = b;
+			b = r;
+		}
+	        LCM = tmp1* tmp2/a; 
+	        return LCM;
+	        
+	}
+	
+}
+
+
